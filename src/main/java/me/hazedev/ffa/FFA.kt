f87b89714c7d13fd16f5d237b74238efde861245
Harry McCormack <mccormack_harry@hotmail.com>
2021-07-31 22:26:08 +0100

    Initial Commit

---
+++ src/main/java/me/hazedev/ffa/FFA.kt
@@ -0,0 +1,15 @@
+package me.hazedev.ffa
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FFA : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
